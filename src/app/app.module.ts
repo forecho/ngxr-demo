26effb9737cfa9d0f75aafdf868d893eaa12719b
forecho <caizhenghai@gmail.com>
2017-09-20 17:12:44 +0800

    准备好所有文件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './core/app.component';
+import {AppComponent} from './core/app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {CreateComponent} from './pages/create/create.component';
+import {CompleteComponent} from './pages/complete/complete.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
